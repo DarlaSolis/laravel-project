795e2bf5054218fc91435e417ab56d662220c86b
DarlaSolis <darladsolis2@gmail.com>
2025-10-13 17:22:36 -0600

    feat(sidebar): add dynamic submenu

--- doctor-appointment-app/resources/views/layouts/includes/admin/sidebar.blade.php
+++ doctor-appointment-app/resources/views/layouts/includes/admin/sidebar.blade.php
@@ -55,7 +55,7 @@
                                 <i class="{{$link['icon']}}"></i>
                             </span>
                                 <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{{ $link['name'] }}</span>
-                                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                <svg class="w-3  h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                                 </svg>
                             </button>
